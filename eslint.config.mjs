f3419be417963690b00f27a027d9a71089b59141
mpr <marinpericradan1@gmail.com>
2025-06-07 21:01:27 +0200

    chore: small eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin"
-import tsParser from "@typescript-eslint/parser"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
-import js from "@eslint/js"
-import { FlatCompat } from "@eslint/eslintrc"
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -15,15 +15,15 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ["**/playwright-report/"],
+    ignores: ['**/playwright-report/'],
   },
   ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
   ),
   {
     plugins: {
-      "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
 
     languageOptions: {
@@ -31,10 +31,11 @@ export default [
     },
 
     rules: {
-      semi: ["error", "never"],
-      "no-unexpected-multiline": ["error"],
-      "eol-last": ["error", "always"],
-      indent: ["error", 2],
+      semi: ['error', 'never'],
+      'no-unexpected-multiline': ['error'],
+      'eol-last': ['error', 'always'],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
     },
   },
 ]
